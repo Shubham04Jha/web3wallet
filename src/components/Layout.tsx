fb26e82e3618d8cb35d346427db4b45cc819b02f
Shubham04Jha <shubhamjha70491@gmail.com>
2025-12-17 19:44:23 +0530

    screen height

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,8 +2,8 @@ import { Header } from "./Header";
 import { Footer } from "./Footer";
 export const Layout = ({children}:{children?: React.ReactNode})=>{
     return (
-        <div className='bg-navy-900  px-4 pt-4 min-h-screen '>
-            <div className="flex flex-col max-w-240 min-h-screen mx-auto h-full">
+        <div className='bg-navy-900 min-h-screen '>
+            <div className="flex flex-col px-4 pt-4 max-w-240 min-h-screen mx-auto h-full">
                 <Header/>
                 <div className="grow ">
                     {children}
